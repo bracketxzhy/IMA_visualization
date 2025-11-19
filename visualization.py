3cdc32ad040ad0b507a12652874b675e3ec3f54b
Heyang Zhang <bracketx@163.com>
2025-11-19 23:09:14 +0800

    first commit

---
+++ visualization.py
@@ -0,0 +1,12 @@
+import pydicom
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+
+file_path = Path('data/ANON_0001.IMA')             
+
+ds = pydicom.dcmread(file_path)                 
+
+plt.imshow(ds.pixel_array, cmap='gray')         
+plt.axis('off')                                 
+plt.show()
